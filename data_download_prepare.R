77110e143125289d772e0df48c565ed0463e137a
SCORPIONVNZLA <joseltg7@gmail.com>
2016-09-11 21:02:45 -0400

    Add files via upload

---
+++ data_download_prepare.R
@@ -0,0 +1,18 @@
+if(!file.exists("exdata-data-household_power_consumption.zip")) {
+  temp <- tempfile()
+  download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+  file <- unzip(temp)
+  unlink(temp)
+}
+data<- read.table(file, header=T, sep=";")
+
+data <- read.table(file, header=T, sep=";")
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+df <- data[(data$Date=="2007-02-01") | (data$Date=="2007-02-02"),]
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
+df$Voltage <- as.numeric(as.character(df$Voltage))
+df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
